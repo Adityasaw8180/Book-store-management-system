397aa64a237c810f901f84b5a9d31e7b68852e92
Aditya Sawant <sawantaditya086@gmail.com>
2025-11-06 19:40:33 +0530

    full-stack project completed successfully

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const pgp = require("pg-promise")();
 const db = pgp({
   host: "localhost", // your DB host
   port: 5432, // default port
-  database: "bookStore", // your DB name
+  database: "bookstore", // your DB name
   user: "postgres", // your username
-  password: "#setup07", // your password
+  password: "help", // your password
 });
 
 module.exports = db;
